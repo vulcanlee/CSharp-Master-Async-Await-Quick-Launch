5d26638476d9420e159fc126eb6ea757e5847a08
Vulcan <vulcan.lee@gmail.com>
2022-07-20 13:54:43 +0800

    AA09|使用 async await 進行要求資料 - 非同步設計

---
+++ CSharpAsyncAwait/AA09 使用 async await 進行要求資料 - 非同步設計/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AA09_使用_async_await_進行要求資料___非同步設計
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        static string baseURI =
+        "https://backendhol.azurewebsites.net/api/performance";
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            string endPoint = $"{baseURI}/add/1/3/3000";
+            var sum = await client.GetStringAsync(endPoint);
+            Console.WriteLine($"兩數相加={sum}");
+            endPoint = $"{baseURI}/Checksum/{sum}/5000";
+            var checksum = await client.GetStringAsync(endPoint);
+            Console.WriteLine($"檢查碼={checksum}");
+        }
+    }
+}
