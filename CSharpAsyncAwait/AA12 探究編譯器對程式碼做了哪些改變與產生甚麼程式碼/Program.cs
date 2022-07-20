e74c10d6326b5b6818d510d57b7b5f1e7789b915
Vulcan <vulcan.lee@gmail.com>
2022-07-21 01:47:06 +0800

    AA12|探究 編譯器 對程式碼做了哪些改變與產生甚麼程式碼

---
+++ CSharpAsyncAwait/AA12 探究編譯器對程式碼做了哪些改變與產生甚麼程式碼/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AA12_探究編譯器對程式碼做了哪些改變與產生甚麼程式碼
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string answer = await MyMethodAsync(123);
+            Console.WriteLine(answer);
+        }
+        static async Task<string> MyMethodAsync(int myInt)
+        {
+            string myString = "Begin";
+            string result = await new HttpClient().GetStringAsync(
+              "https://lobworkshop.azurewebsites.net/api/" +
+              "RemoteSource/Add/8/9/2");
+            myString = "Complete" + myInt + " " + result;
+            return myString;
+        }
+    }
+}
