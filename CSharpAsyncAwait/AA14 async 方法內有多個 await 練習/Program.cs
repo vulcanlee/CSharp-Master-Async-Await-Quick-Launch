b29c8036860429297330767664a5dff3989002e5
Vulcan <vulcan.lee@gmail.com>
2022-07-21 12:22:13 +0800

    AA14|async 方法內有多個 await 練習

---
+++ CSharpAsyncAwait/AA14 async 方法內有多個 await 練習/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AA14_async_方法內有多個_await_練習
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string localString = "區域變數";
+            int localInt = 0;
+            Console.WriteLine($"Thread : {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Run(() => Thread.Sleep(1000));
+            localString += localInt + 1;
+            Console.WriteLine($"Thread : {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Run(() => Thread.Sleep(500));
+            localString += localInt + 2;
+            Console.WriteLine($"Thread : {Thread.CurrentThread.ManagedThreadId}");
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
