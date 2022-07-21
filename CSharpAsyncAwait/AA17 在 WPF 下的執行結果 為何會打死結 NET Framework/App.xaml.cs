c1c23dfe3c2f4de5cf363db24422947eb200dc86
Vulcan <vulcan.lee@gmail.com>
2022-07-21 14:36:23 +0800

    AA17|在 WPF 下的執行結果 為何會打死結

---
+++ CSharpAsyncAwait/AA17 在 WPF 下的執行結果 為何會打死結 NET Framework/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AA17_在_WPF_下的執行結果_為何會打死結_NET_Framework
+{
+    /// <summary>
+    /// App.xaml 的互動邏輯
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ CSharpAsyncAwait/AA17 在 WPF 下的執行結果 為何會打死結 NET Framework/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AA17_在_WPF_下的執行結果_為何會打死結_NET_Framework
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void ShowThreadId(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+                Debug.WriteLine("");
+            else
+                Debug.WriteLine($"{message} Thread Id=" +
+                    $"{Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        async Task MyAsyncMethod()
+        {
+            ShowThreadId("await MyMethod Before");
+            await Task.Delay(1000);
+            ShowThreadId("await MyMethod After");
+            return;
+        }
+        Task MyPureTask()
+        {
+            return Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+            });
+        }
+
+        async void await非同步工作_Click(object sender, RoutedEventArgs e)
+        {
+            var myTask = MyPureTask();
+            ShowThreadId("await myTask Before");
+            await myTask;
+            ShowThreadId("await myTask After");
+            ShowThreadId("");
+        }
+        async void await非同步方法_Click(object sender, RoutedEventArgs e)
+        {
+            var myTask = MyAsyncMethod();
+            ShowThreadId("await myTask Before");
+            await myTask;
+            ShowThreadId("await myTask After");
+            ShowThreadId("");
+        }
+        void Wait非同步工作_Click(object sender, RoutedEventArgs e)
+        {
+            var myTask = MyPureTask();
+            ShowThreadId("Wait myTask Before");
+            myTask.Wait();
+            ShowThreadId("Wait myTask After");
+            ShowThreadId("");
+        }
+
+        void Wait非同步方法_Click(object sender, RoutedEventArgs e)
+        {
+            var myTask = MyAsyncMethod();
+            ShowThreadId("Wait myTask Before");
+            myTask.Wait();
+            ShowThreadId("Wait myTask After");
+            ShowThreadId("");
+        }
+    }
+}
