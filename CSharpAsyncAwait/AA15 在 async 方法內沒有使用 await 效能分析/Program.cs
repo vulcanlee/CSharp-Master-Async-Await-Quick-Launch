e48dd0dfa724a07e0c1ba16c7ee1fe0dd3921aa7
Vulcan <vulcan.lee@gmail.com>
2022-07-21 13:13:21 +0800

    AA16|非同步 Task 在執行中，不一定會用到執行緒的範例

---
+++ CSharpAsyncAwait/AA15 在 async 方法內沒有使用 await 效能分析/Program.cs
@@ -0,0 +1,77 @@
+﻿#define 同步方法
+//#define 空的非同步方法
+//#define 立即返回非同步方法
+
+namespace AA15_在_async_方法內沒有使用_await_效能分析
+{
+    using System.Diagnostics;
+
+    class Program
+    {
+#if 同步方法
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                string foo = 同步方法();
+            }
+            sw.Stop();
+            double cost = sw.Elapsed.TotalMilliseconds / 1000.0;
+            Console.WriteLine($"呼叫 1000 次 同步方法 的平均花費時間 : {cost} ms");
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+
+        // 底下是一個 同步方法
+        static string 同步方法()
+        {
+            return "同步方法";
+        }
+#elif 空的非同步方法
+        static async Task Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                string foo = await 空的非同步方法();
+            }
+            sw.Stop();
+            double cost = sw.Elapsed.TotalMilliseconds / 1000.0;
+            Console.WriteLine($"呼叫 1000 次 同步方法 的平均花費時間 : {cost} ms");
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+
+        // 底下是一個 非同步方法 ，方法內僅有一個立即返回敘述
+        static async Task<string> 空的非同步方法()
+        {
+            return "同步方法";
+        }
+#elif 立即返回非同步方法
+        static async Task Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                string foo = await 立即返回非同步方法();
+            }
+            sw.Stop();
+            double cost = sw.Elapsed.TotalMilliseconds / 1000.0;
+            Console.WriteLine($"呼叫 1000 次 同步方法 的平均花費時間 : {cost} ms");
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+
+        // 底下是一個 也是一個非同步方法，不過會先呼叫 await 後，再返回
+        static async Task<string> 立即返回非同步方法()
+        {
+            await Task.Yield();
+            return "同步方法";
+        }
+#endif
+    }
+}
---
+++ CSharpAsyncAwait/AA16 非同步 Task 在執行中，不一定會用到執行緒的範例/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace AA16_非同步_Task_在執行中_不一定會用到執行緒的範例
+{
+    internal class Program
+    {
+        static string endPoint =
+                 "https://backendhol.azurewebsites.net/api/DoAsyncWork/";
+        static async Task Main(string[] args)
+        {
+            // 宣告一個集合物件，儲存這五個非同步工作物件
+            List<Task<string>> allTasks = new();
+            for (int i = 0; i < 5; i++)
+            {
+                var client = new HttpClient();
+                var newEndPoint = $"{endPoint}/{5000 + i * 300}";
+                // 取得使用 HttpClient 呼叫遠端 Web API 的非同步工作物件
+                // 這裡的工作物件屬於 I/O Bound 密集的非同步工作
+                var task = client.GetStringAsync(newEndPoint);
+                var continueTask = task.ContinueWith(t =>
+                {
+                    // 在這裡設定中斷點，觀察 [執行緒] / [工作] 視窗內容
+                    // 觀察是否有使用到執行緒
+                    var checkBreakPoint = 100;
+                });
+                allTasks.Add(task);
+            }
+
+            // 主執行緒強制休息一秒鐘，讓非同步工作已經順利開始執行了
+            await Task.Delay(1000);
+            // 在這裡設定中斷點，觀察 [執行緒] / [工作] 視窗內容
+            // 觀察是否有使用到執行緒
+            var checkBreakPoint = 100;
+            Thread.Sleep(10000);
+        }
+    }
+}
