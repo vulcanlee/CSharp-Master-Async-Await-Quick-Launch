ef3bc5185344eb868c6bf5d4b6caae1da8416251
Vulcan <vulcan.lee@gmail.com>
2022-07-21 12:00:33 +0800

    AA13|反組譯 查看 迭代器 Iterator 實作作法

---
+++ CSharpAsyncAwait/AA13 反組譯 查看 迭代器 Iterator 實作作法/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AA13_反組譯_查看_迭代器_Iterator_實作作法
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int number in SomeNumbers())
+            {
+                Console.Write(number.ToString() + " ");
+            }
+            // Output: 3 5 8
+            Console.ReadKey();
+        }
+        public static System.Collections.IEnumerable SomeNumbers()
+        {
+            yield return 3;
+            yield return 5;
+            yield return 8;
+        }
+    }
+}
