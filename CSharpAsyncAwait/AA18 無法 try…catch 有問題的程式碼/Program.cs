2a848a138e0ce1999b723e4d11cb5dc66b5ab991
Vulcan <vulcan.lee@gmail.com>
2022-07-22 12:01:10 +0800

    AA18|無法 try…catch 有問題的程式碼

---
+++ CSharpAsyncAwait/AA18 無法 try…catch 有問題的程式碼/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AA18_無法_try_catch_有問題的程式碼
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MethodAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetType().Name);
+            }
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+        static async void MethodAsync()
+        {
+            await Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                throw new ArgumentNullException("引數空值例外異常產生了");
+            });
+        }
+    }
+}
