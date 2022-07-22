ec1d7753b37af4b90ecaf4e894b06a7547e58f46
Vulcan <vulcan.lee@gmail.com>
2022-07-22 13:09:12 +0800

    AA19|程式突然當掉，卻不知道問題在哪

---
+++ CSharpAsyncAwait/AA19 程式突然當掉，卻不知道問題在哪 NET Framework/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AA19_程式突然當掉_卻不知道問題在哪_NET_Framework
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await MethodAsync();
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+
+        static async Task MethodAsync()
+        {
+            await MethodTaskAsync();
+            throw new NotImplementedException("喔喔，系統內部發生問題");
+        }
+
+        static Task MethodTaskAsync()
+        {
+            return Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                throw new NotImplementedException("強制拋出例外異常");
+            });
+        }
+    }
+}
