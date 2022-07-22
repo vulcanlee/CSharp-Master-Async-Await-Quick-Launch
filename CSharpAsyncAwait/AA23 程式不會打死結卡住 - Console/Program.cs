8b520d92fe82df04efb8e420414b0a2191e5f584
Vulcan <vulcan.lee@gmail.com>
2022-07-22 14:11:41 +0800

    AA23|程式不會打死結卡住 - Console

---
+++ CSharpAsyncAwait/AA23 程式不會打死結卡住 - Console/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AA23_程式不會打死結卡住___Console
+{
+    internal class Program
+    {
+        static string baseURI =
+            "https://backendhol.azurewebsites.net/api/performance";
+        static void Main(string[] args)
+        {
+            var sumTask = SumAsync(168, 89);
+            var result = sumTask.Result;
+            Console.WriteLine(result);
+        }
+        static async Task<string> SumAsync(int a, int b)
+        {
+            var client = new HttpClient();
+            var endPoint = $"{baseURI}/add/{a}/{b}/2";
+            var task = client.GetStringAsync(endPoint);
+            var result = await task;
+            return result;
+        }
+    }
+}
